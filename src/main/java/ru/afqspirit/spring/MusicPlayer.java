d9d6d72ccc39a9ff4680fd924640b032617c42a6
afqspirit <sanek.zhuk38@gmail.com>
2019-07-24 11:15:22 +0300

    six

--- src/main/java/ru/afqspirit/spring/MusicPlayer.java
+++ src/main/java/ru/afqspirit/spring/MusicPlayer.java
@@ -3,6 +3,24 @@ package ru.afqspirit.spring;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     //IoC
     public MusicPlayer(Music music){
