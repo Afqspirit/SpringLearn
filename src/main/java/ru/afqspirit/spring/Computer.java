47496d298e2d1bc62f5ea16321ebc1559508dedb
afqspirit <sanek.zhuk38@gmail.com>
2019-07-24 12:48:47 +0300

    Аннотация @Autowired

---
+++ src/main/java/ru/afqspirit/spring/Computer.java
@@ -0,0 +1,25 @@
+package ru.afqspirit.spring;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Computer {
+    private int id;
+    private MusicPlayer musicPlayer;
+
+    public Computer(MusicPlayer musicPlayer) {
+        this.id = 1;
+        this.musicPlayer = musicPlayer;
+    }
+
+    @Autowired
+    public String toString() {
+        return "Computer{" +
+                "id= " + id +
+                ", musicPlayer= " + musicPlayer +
+                '}';
+    }
+
+}
--- src/main/java/ru/afqspirit/spring/RapMusic.java
+++
@@ -1,8 +0,0 @@
-package ru.afqspirit.spring;
-
-public class RapMusic implements Music {
-    @Override
-    public String getSong() {
-        return "99 problems";
-    }
-}
