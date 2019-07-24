b8aa867ed7b8c98f4d28ee061d3aaf4408dad6ec
afqspirit <sanek.zhuk38@gmail.com>
2019-07-24 12:01:56 +0300

    Жизненный цикл бинов

--- src/main/java/ru/afqspirit/spring/ClassicalMusic.java
+++ src/main/java/ru/afqspirit/spring/ClassicalMusic.java
@@ -1,6 +1,21 @@
 package ru.afqspirit.spring;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){}
+
+    public  static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Делаю мою инициализацию");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Совершаю уничтожение этого бина");
+    }
+
+
     @Override
     public String getSong(){
         return "Bohemian Rhapsody";
